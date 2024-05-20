e9b5f0450c024a295a153adf1f75020837001a4b
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-20 11:01:58 +0400

    Add project files.

---
+++ WebApi.Tests/ProductsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.Net;
+using WebApiForCICD;
+
+namespace WebApi.Tests
+{
+    [TestFixture]
+    public class ProductsControllerTests
+    {
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+
+                });
+            });
+
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task GetProducts_ReturnsOkResponse()
+        {
+            var response = await _client.GetAsync("/api/products");
+            response.EnsureSuccessStatusCode();
+            Assert.Equals(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ WebApiForCICD/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiForCICD.Entities;
+using WebApiForCICD.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiForCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var products = _productService.GetProducts();
+            return products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            var product = _productService.GetProductById(id);
+            return product;
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public Product Post([FromBody] Product product)
+        {
+            _productService.Add(product);
+            return product;
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product product)
+        {
+            _productService.Update(product);
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _productService.Delete(id);
+        }
+    }
+}
